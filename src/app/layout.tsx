7f831dc276023d93ec22d15ef5dde6c01f19c45a
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-29 19:48:41 +0530

    QueryProvider function has been added in layout of the project...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "@/providers/QueryProvider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <QueryProvider>
+          {children}
+          <Toaster position="top-right" />
+        </QueryProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center  font-sans ">
+    <div className="min-h-screen bg-slate-500 text-white flex flex-col items-center justify-center  font-sans ">
       <h1 className="text-3xl">Hello Waiters! Good news..</h1>
       <p className="mt-4">ShaadiBio is coming soon!</p>
     </div>
