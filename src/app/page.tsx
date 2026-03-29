e647cb797eb962c66926e88ce20b63df535a1d70
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-29 18:47:44 +0530

    Some text added in master branch...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center  font-sans ">
-      <h1>Hello Shaadibio is coming soon...</h1>
+      <h1 className="text-3xl">Hello Waiters! Good news..</h1>
+      <p className="mt-4">ShaadiBio is coming soon!</p>
     </div>
   );
 }
