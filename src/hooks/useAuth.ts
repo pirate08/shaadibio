694079a45e8563566f3e7db03d6a24880c1c30f1
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-30 23:15:32 +0530

    Api details fetched successfully...

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -18,6 +18,11 @@ export const Register = () => {
         },
         body: JSON.stringify(data),
       });
+      const json = await res.json();
+
+      if (!res.ok) throw new Error(json.message || "Registration failed");
+
+      return json;
     },
   });
 };
