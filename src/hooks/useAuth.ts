18eba700f4aff9067755574587be9b4eeeeefbb1
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-30 23:12:11 +0530

    Api added for the Post function call for register function...

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,3 +3,21 @@ import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
 import { useAuthStore } from "@/stores/useAuthStore";
 import { RegisterData, LoginData } from "@/types/auth";
+
+// --Register function goes here--
+export const Register = () => {
+  const { setAuth } = useAuthStore();
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: async (data: RegisterData) => {
+      const res = await fetch(`${process.env.BASE_URL}/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    },
+  });
+};
