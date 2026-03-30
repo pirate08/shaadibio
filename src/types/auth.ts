763fa7e5e08a1d077725df5fd941dfca8673aaff
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-30 19:21:31 +0530

    Added action functions in the Auth State type file...

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -4,4 +4,8 @@ export interface AuthState {
   user: UserProps | null;
   token: string | null;
   isAuthenticated: boolean;
+
+  setAuth: (user: UserProps, token: string) => void;
+  logout: () => void;
+  updateUser: (data: Partial<UserProps>) => void;
 }
