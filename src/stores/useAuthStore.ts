aa6a9dbe994307014ec803389b196ef209c87c8f
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-30 19:24:52 +0530

    Added setAuth function in useAuthStore function...

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,3 +1,15 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { AuthState } from "@/types/auth";
+
+export const useAuthStore = create<AuthState>()(
+  persist((set) => ({
+    user: null,
+    token: null,
+    isAuthenticated: false,
+
+    setAuth: (user, token) => {
+      set({ user, token, isAuthenticated: true });
+    },
+  })),
+);
