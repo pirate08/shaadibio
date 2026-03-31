0d1950e296d7e1d8449478146ebc2124198015cb
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-31 19:42:47 +0530

    Added state and validation for login form...

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import LoginUi from "@/components/auth/LoginUi";
+import { useLogin } from "@/hooks/useAuth";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const Login = () => {
   return (
