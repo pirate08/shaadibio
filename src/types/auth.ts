db28b30376c7d73bdcec886325172974607ab1a4
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-31 16:42:57 +0530

    Register Form data type has been created...

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -20,3 +20,10 @@ export interface LoginData {
   email: string;
   password: string;
 }
+
+export interface RegisterFormdata {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
