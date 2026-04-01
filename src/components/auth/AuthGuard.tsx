291ef13dcbf19b677ee6b16e7fde9e07f45625b9
Mayukh <mayukhdebgoswami82@gmail.com>
2026-04-01 18:42:39 +0530

    Work in progress in Auth Gurad file...

---
+++ src/components/auth/AuthGuard.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const AuthGuard = () => {
+  return (
+    <div>
+      <div>
+        <h1>Working in progress..</h1>
+      </div>
+    </div>
+  );
+};
+
+export default AuthGuard;
