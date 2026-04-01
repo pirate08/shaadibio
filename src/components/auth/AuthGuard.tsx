3e6c09adf3d6d86acfec0accf9bb27720486af33
Mayukh <mayukhdebgoswami82@gmail.com>
2026-04-01 18:49:14 +0530

    Auth Gurad function is fully ready...

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,12 +1,22 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { Children, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/stores/useAuthStore";
 
-const AuthGuard = () => {
+const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated } = useAuthStore();
   const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.replace("/login");
+    }
+  }, [isAuthenticated, router]);
+
+  if (isAuthenticated) return null;
+
+  return <>{children}</>;
 };
 
 export default AuthGuard;
