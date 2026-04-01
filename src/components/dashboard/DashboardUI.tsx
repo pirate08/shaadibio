29332474af2aaca6921ce5e8498f831e7ea03250
Mayukh <mayukhdebgoswami82@gmail.com>
2026-04-01 18:28:49 +0530

    Dashboard Ui page has been created...

---
+++ src/components/dashboard/DashboardUI.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardUI = () => {
+  return (
+    <div>
+      <h1>Welcome Back! You are in Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardUI;
