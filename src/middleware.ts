cb5a9efa2b2611cc2907ff31e9645d6af68723ed
Mayukh <mayukhdebgoswami82@gmail.com>
2026-04-04 17:03:18 +0530

    Fetched token and define protected and public routes in the middleware file...

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,2 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // --Get the token from the cookie--
+  const token = request.cookies.get("token")?.value;
+  const { pathname } = request.nextUrl;
+
+  //   --Define protected and public routes--
+  const isDashboardPage = pathname.startsWith("/dashboard");
+  const isAuthPage = pathname === "/login" || pathname === "/register";
+}
